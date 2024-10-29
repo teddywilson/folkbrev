8bb61702bc62c56e10b0705492873d4ef9dbe33e
Teddy Wilson <hello@teddywilson.me>
2024-10-29 13:02:01 +0100

    first stitch effort to clean up content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
-import Divider from './components/Divider';
-import StickyContainer from './components/StickyContainer';
-import './App.css';
 import StickyBox from 'react-sticky-box';
 import { CSSTransition } from 'react-transition-group';
+
+import './App.css';
+
+import StickyContainer from './components/StickyContainer';
+import { content } from './content';
 
 const relaxedSubheadingWeight = 200;
 // const acceptedNoticedKey = "acceptedNotice";
@@ -30,13 +32,13 @@ function App() {
       <CSSTransition nodeRef={nodeRef} in={!acceptedNotice} timeout={200} classNames="header" unmountOnExit>
         <StickyBox>
           <div ref={nodeRef} className='header'>
-            <h4>Innehåll och information om Folkbrev får inte på något sätt delas på Instagram, Facebook, etc.</h4>
-            <div onClick={onClick} className="button">OK</div>
+            <h4>{content.header.text}</h4>
+            <div onClick={onClick} className="button">{content.header.button}</div>
           </div>
         </StickyBox>
       </CSSTransition>
       <StickyContainer>
-        <h1>FOLKBREV</h1>
+        <h1>{content.landing.title}</h1>
         <h2 style={{
           fontFamily: '"Cormorant Garamond", serif'
         }}>
@@ -48,46 +50,11 @@ function App() {
           </i>
         </h2>
       </StickyContainer>
-      <StickyContainer>
-        <h2>Tack för att du är här - vi vill hjälpa dig att hjälpa!</h2>
-        <h3>{`\n`}</h3>
-        <Divider />
-        <h3>{`
-För känner du också hopplöshet kring världsläget och dina möjligheter att påverka? Det pågår ett folkmord i livesändning, Sveriges regering är tyst, och flera svenska företag, institutioner och medier är direkt eller indirekt delaktiga i Israels ockupation av Gaza och krigföring i Mellanöstern.\n
-Folkbrev är ett snabbt och konkret sätt att engagera dig och göra skillnad, genom att sätta press på makthavare och opinionsbildare. Det tar 2-4 minuter och är en chans att faktisk påverka.\n
-Ju fler vi blir, desto starkare blir budskapet, vilket kan leda till verklig förändring!
-        `}
-        </h3>
-      </StickyContainer>
-      <StickyContainer>
-        <h2>Vad är ett Folkbrev?</h2>
-        <h3>{`\n`}</h3>
-        <Divider />
-        <h3>{`
-Ett Folkbrev är ett AI-genererat personaliserat mejl som skickas till utvalda beslutsfattare och opinionsbildare inom politik, media och näringsliv.\n 
-I och med att innehållet personanpassas (du kommer snart förstå hur) kommer vi tillsammans kunna skicka ut mejl i stor skala, med liten risk för att det det fastnar i spamfilter eller kan sållas bort som “kedjemejl” av mottagaren.\n
-Vi tror att ju fler mejl som skickas till dessa personer, desto större effekt kan vi få. Vi vill göra det omöjligt att ignorera oss.\n
-Följ några snabba stegen nedan för att göra din röst hörd på ett effektivt sätt. Tillsammans kan vi påverka beslut som påverkar liv!        
-`}
-        </h3>
-      </StickyContainer>
-      <StickyContainer>
-        <h2>Men först: vem vill du skicka ett Folkbrev till?</h2>
-        <h3>{`\n`}</h3>
-        <Divider />
-        <h3>{`
-Klicka på namnet på den du vill kontakta i listan nedan, så öppnas ett nytt utkast i ditt mejlverktyg. Fungerar inte det – kopiera mejladressen och klistra in i ett nytt mejl på egen hand.\n
-Om du vill skicka till fler går det självklart bra – the more the merrier!
-        `}
-        </h3>
-      </StickyContainer>
-      <StickyContainer>
-        <h2>Skapa ditt unika Folkbrev!</h2>
-        <h3>{`\n`}</h3>
-        <Divider />
-        <h3>{`
-Läs igenom dom här instruktionerna innan du går vidare
-        `}</h3>
+      <StickyContainer heading={content.block1.heading} text={content.block1.text} />
+      <StickyContainer heading={content.block2.heading} text={content.block2.text} />
+      <StickyContainer heading={content.block3.heading} text={content.block3.text} />
+      {/* TODO(teddywilson): generalize content generation more after direction clear. */}
+      <StickyContainer heading={content.block4.heading} text={content.block4.text}>
         <ol>
           <li>
             Gå in på <a href="https://writemail.ai/">https://writemail.ai/</a>.
@@ -107,9 +74,7 @@ Läs igenom dom här instruktionerna innan du går vidare
           <li>Kom sen tillbaka hit för ett sista litet men viktigt steg.</li>
         </ol>
       </StickyContainer>
-      <StickyContainer>
-        <h2>Markera vem/vilka nedan som du har skickat ett mail till.</h2>
-      </StickyContainer>
+      <StickyContainer heading={content.block5.text} />
     </div >
   );
 }
--- src/components/StickyContainer.js
+++ src/components/StickyContainer.js
@@ -1,10 +1,19 @@
 import React, { useRef } from 'react';
+import Divider from './Divider';
 
-const StickyContainer = ({ children }) => {
+const StickyContainer = ({ heading, text, children }) => {
   const containerRef = useRef(null);
 
   return (
     <div ref={containerRef} className="sticky-container">
+      {heading && <h2>{heading}</h2>}
+      {(heading && text) &&
+        <>
+          <h3>{`\n`}</h3>
+          <Divider />
+        </>
+      }
+      {text && <h3>{text}</h3>}
       {children}
     </div>
   );
